55cfd78f5fba53aa580ef8786453272120d37d2d
Dragon116rus <ARBogdanov@stud.kpfu.ru>
2019-04-15 23:00:28 +0300

    added data collector

---
+++ data_collector.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import json
+from image_checker import ImageChecker
+try:
+    from tqdm import tqdm as tqdm
+except:
+    tqdm = lambda x : x
+
+def get_imagenet_labels():
+    with open('imagenet_class_index.json', 'r') as f:
+        class_idx = json.load(f)
+    imagenet_labels = [class_idx[str(k)][1] for k in range(len(class_idx))]
+    return imagenet_labels
+
+def get_binary_images(label):
+    query = label.replace("_", "%20")
+    content = requests.get("https://www.google.ru/search?q={}&tbm=isch".format(query)).content
+    soup = BeautifulSoup(content)
+    table_row = soup.find('table', {'class':'images_table'}).tr
+    for number, td in enumerate(table_row):
+        img_src = (td.img.attrs['src'])
+        img_binary = requests.get(img_src).content
+        yield img_binary
+
+def create_dir(name):
+    if not os.path.isdir(name):
+        os.mkdir(name)
+
+def save_jpg(img_binary, dir_name, file_number):
+    with open(os.path.join(dir_name, "img{}.jpg".format(file_number)), "wb") as f:
+        f.write(img_binary)
+
+if __name__ == "__main__":
+    checker = ImageChecker()
+    imagenet_labels = get_imagenet_labels()
+    for label_num, label in tqdm(enumerate(imagenet_labels)):
+        create_dir(label)
+        for i, img_binary in enumerate(get_binary_images(label)):
+            if checker.check(img_binary, label_num):
+                save_jpg(img_binary, dir_name = label, file_number = i)
+                print(label, i)
---
+++ image_checker.py
@@ -0,0 +1,37 @@
+import cv2
+import torch
+import torchvision.datasets as datasets
+import torchvision.models as models
+import torchvision.transforms as transforms
+import numpy as np
+
+class ImageChecker:
+    def __init__(self):
+        resnet50 = models.resnet50(pretrained=True)
+        resnet50.eval()
+        alexnet = models.alexnet(pretrained=True)
+        vgg16 = models.vgg16(pretrained=True)
+        densenet = models.densenet161(pretrained=True)
+        densenet.eval()
+        self.models = [densenet, resnet50, vgg16, alexnet]
+        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+    
+    def check(self, img_binary, label_ind):
+        img_tensor = self.preprocess(img_binary)
+        for model in self.models:
+            predict = model(img_tensor)[0].argmax()
+            if predict != label_ind:
+                return False
+        return True
+
+    def preprocess(self, img_binary):
+        bgr_img = cv2.imdecode(np.frombuffer(img_binary, dtype=np.uint8), flags=cv2.IMREAD_UNCHANGED)
+        bgr_img = cv2.resize(bgr_img, (224, 224)) 
+        rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
+        img = rgb_img
+        img = img.swapaxes(0, 1).swapaxes(0, 2) / 255
+        tensor = torch.from_numpy(img)
+        tensor = self.normalize(tensor.float())
+        tensor = tensor.unsqueeze(0)
+        return tensor
