2b815f72ffc24c4c71c9851342fa484c61df1f22
Dragon116rus <ARBogdanov@stud.kpfu.ru>
2019-04-16 23:39:45 +0300

    added self-made differantial evolution for discrete case

---
+++ differantial_evolution.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def generate_population(bounds_min, bounds_max, population_size):
+  population = np.zeros(shape=(population_size, len(bounds_min)), dtype=np.int32)
+  for i in range(len(bounds_min)):
+    population[:, i] = np.random.randint(bounds_min[i], bounds_max[i], size=population_size)
+  return population
+
+def check_bounds(array, bounds_min, bounds_max):
+  return np.maximum(bounds_min, np.minimum(array, bounds_max))
+
+def diff_evaluation(score, bounds_min, bounds_max):
+  
+  max_iters = 100
+  count = 0
+
+  population_size = 100
+  f = lambda x: x
+  all_possible_indices = np.arange(population_size)
+  genes_size = len(bounds_min)
+  population = generate_population(bounds_min, bounds_max, population_size) #np.random.randint(-5, 5, size=genes_size*population_size).reshape(population_size, genes_size)
+  crossover_p = 0.5
+  while count < max_iters: 
+    for individual_ind in range(population_size):
+      # get random indices to mutate
+      individual = population[individual_ind]
+      random_indices = np.random.choice(all_possible_indices, 4, replace=False)
+      if individual in random_indices:
+        ind1, ind2, ind3 = random_indices[random_indices!=individual_ind]
+      else:
+        ind1, ind2, ind3 = random_indices[:3]
+      # mutation
+      donor = population[ind1] + f(population[ind2] - population[ind3])
+      donor = check_bounds(donor, bounds_min, bounds_max)
+
+      # crossover
+      binomial = np.random.binomial(1, crossover_p, genes_size)
+      trial = binomial*donor + (1 - binomial) * individual
+
+      # selection
+      if score(trial) > score(individual):
+        population[individual_ind] = trial
+    count+=1
+  final_scores = np.apply_along_axis(score, 1, population)
+  return population[final_scores.argmax()], final_scores.argmax()
+  # population
